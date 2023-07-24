a4c7a770c6663f4bba1994eb875f571e830172dd
Ashish Mahajan <aashishmahajan01@gmail.com>
2023-07-24 21:38:55 +0530

    .

---
+++ Coding/src/String/AA.java
@@ -0,0 +1,19 @@
+package String;
+
+public class AA {
+
+	public static void main(String[] args) {
+		int a = 1;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b;
+        d = a++;
+        c++;
+        b++;
+        ++a;
+        System.out.println(a + " " + b + " " + c);
+
+	}
+
+}
