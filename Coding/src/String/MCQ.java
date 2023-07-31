b7255ed4f63a17fcfd646fde98be2ac6fbde5c22
Ashish Mahajan <aashishmahajan01@gmail.com>
2023-07-31 22:13:14 +0530

    mcq prac

---
+++ Coding/src/String/MCQ.java
@@ -0,0 +1,23 @@
+package String;
+
+public class MCQ {
+	public static void main(String[] args) {
+		/*int x=12;
+		int y=6;
+		System.out.println(x-- +" "+ --x);
+		System.out.println(x-- - --x);
+		int z=x-- - --x + ++y +y++;
+		System.out.println(z);
+	*/
+		int a =1;
+		int b=2;
+		int c=3;
+		System.out.println(1|5);
+		a |=5;
+		b >>=1;
+		c <<=1;
+		a^=c;
+		System.out.println(a+" "+b+" "+c+" ");
+	}
+
+}
---
+++ JavaProject/src/MCQ/Array.java
@@ -0,0 +1,11 @@
+package MCQ;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] a = {120,200,016};
+        for(int e :a) {
+            System.out.print(e+ " ");
+        }
+    }
+
+}
